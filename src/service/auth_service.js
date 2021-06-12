b279f29ecee763a5c524060ff155896ac7f25ccf
Lim223 <shfks1212@naver.com>
2021-06-12 13:42:26 +0900

    add login and logout functionality

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -6,6 +6,16 @@ class AuthService {
     const authprovider = new firebase.auth[`${providerName}AuthProvider`]();
     return firebaseApp.auth().signInWithPopup(authprovider);
   }
+
+  logout() {
+    firebase.auth().signOut();
+  }
+
+  onAuthChange(onUserChanged) {
+    firebase.auth().onAuthStateChanged(user => {
+      onUserChanged(user);
+    });
+  }
 }
 
 export default AuthService;
