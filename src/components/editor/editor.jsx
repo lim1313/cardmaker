62d20d70dd6a340501d3452c9f2991946d6febb5
Lim223 <shfks1212@naver.com>
2021-06-12 22:11:16 +0900

    add editor and preview layout

---
+++ src/components/editor/editor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./editor.module.css";
+
+const Editor = props => {
+  return (
+    <section className={styles.editor}>
+      <div className={styles.title}>Card Maker</div>
+    </section>
+  );
+};
+
+export default Editor;
